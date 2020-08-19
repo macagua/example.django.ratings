bea3327a479c68632fdaef172b0196ff1f329ec6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-08-19 19:11:18 -0400

    Added destinations model initial

--- mysite/destinations/apps.py
+++ mysite/destinations/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class DestinationsConfig(AppConfig):
     name = 'destinations'
+    verbose_name = _('Destinations')
--- mysite/destinations/models.py
+++ mysite/destinations/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Destinations(models.Model):
+
+    name = models.CharField(
+        _('Name'),
+        max_length=200,
+        blank=True,
+        null=True,
+    )
+
+    short_description = models.TextField(
+        _('Short description'),
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(
+        _('Description'),
+        blank=True,
+        null=True,
+    )
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = _("Destination")
+        verbose_name_plural = _('Destinations')
